738f3d15da42990f86202730d233bf89d56f2067
Vishal Kalola <vishalkalola196@gmail.com>
2021-10-08 16:00:10 +0200

    Default image

--- api/models.py
+++ api/models.py
@@ -13,7 +13,7 @@ class Restaurant(models.Model):
     name = models.CharField(max_length=255, default="")
     address = models.CharField(max_length=255, default="")
     contact = models.CharField(max_length=20, default="")
-    image = models.ImageField(_("Image"), upload_to=upload_to, default='restaurant/default.jpg')
+    image = models.ImageField(_("Image"), upload_to=upload_to, default='restaurant/default.jpeg')
 
     def __str__(self):
         return f"{self.name}"
