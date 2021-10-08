5e66983594d7c966969afc0778b002157a8daf85
Vishal Kalola <vishalkalola196@gmail.com>
2021-10-08 23:10:08 +0200

    change model

--- api/models.py
+++ api/models.py
@@ -12,7 +12,7 @@ class Restaurant(models.Model):
     name = models.CharField(max_length=255, default="")
     address = models.CharField(max_length=255, default="")
     contact = models.CharField(max_length=20, default="")
-    image = models.ImageField(_("Image"), upload_to=upload_to, default='restaurant/default.jpeg')
+    image = models.ImageField(_("Image"), upload_to=upload_to, default='restaurant/default.jpg')
 
     def __str__(self):
         return f"{self.name}"
