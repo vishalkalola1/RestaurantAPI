432e0780934f522c939909d7beb11a5795f0fb6a
Vishal Kalola <vishalkalola196@gmail.com>
2021-10-08 16:05:14 +0200

    changes done

--- api/models.py
+++ api/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
 
 RATING_CHOICES = ((0, 0), (1, 1), (2, 2), (3, 3), (4, 4), (5, 5))
 
